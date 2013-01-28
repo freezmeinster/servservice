be4727d5593d29bceb59747b5eefdc802b8a0f05
Bramandityo Prabowo <freez_meinster@yahoo.co.id>
2013-01-28 16:59:18 +0700

    update mployee model

--- apps/employee/models.py
+++ apps/employee/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Office(models.Model):
+    name = models.CharField(max_length=255)
+    address = models.TextField()
+    phone = models.CharField(max_length=20)
+    
+class Employee(User):
+    EMP = (
+        ("pm","Project Manager"),
+        ("n","NOC"),
+        ("p","Programer"),
+    )
+    
+    position = models.CharField(max_length=2,choices=EMP)
+    office = models.ForeignKey(Office)
+    phone = models.CharField(max_length=20)
--- apps/employee/views.py
+++ apps/employee/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
+from django.contrib.auth.models import User
 
 def index(request):
     return render_to_response("employee/index.html",{
-        
+            "users" : User.objects.all() 
         },context_instance=RequestContext(request))
